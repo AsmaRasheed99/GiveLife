48c90df9c0a3b1bcc46ea105f5a12a95c9dd3859
AsmaRasheed99 <AsmaRasheed4599@gmail.com>
2023-06-20 02:04:40 +0300

    final commiy

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
-const protected =require("../middleware/Protected")
+const protected =require("../middlewares/Protected")
 router.get("/api/users",protected , userController.allUsers);
-router.get("/api/usersMessages",protected , userController.usersMessages);
+router.get("/api/usersMessages",protected , userController.usersMessage);
 router.get("/api/Providers", userController.allProviders);
 router.get("/api/Admins", userController.allAdmins);
 router.get("/protected", userController.protected);
 router.post("/api/users", userController.newUser);
 router.post("/api/usersLogin", userController.newUserLogin);
-router.put("/api/usersContactUs/:id", userController.newUserContactUs);
+router.put("/api/usersContactUs/:id", userController.newUserContactUS);
 router.get("/api/users/:id", userController.oneUser);
 router.put("/api/users/:id", userController.updateUser);
 router.delete("/api/users/:id", userController.deleteUser);
--- server/server.js
+++ server/server.js
@@ -2,20 +2,31 @@ const express = require("express");
 const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 const mongoose = require("mongoose");
-const dbURI = process.env.URI
+const userRouts = require('./routes/userRouter');
+const aboutUsRouts = require('./routes/aboutUsRouter');
+const paymentRouts = require('./routes/paymentRouter');
+const BlogRouts = require('./routes/blogRouter');
+
+const beneficiaryRouter = require('./routes/beneficiaryRouter');
+const dbURI ="mongodb+srv://majdishomali1997:uVxsL6cXyv6CIZv8@cluster0.pacgw6a.mongodb.net/charity"
 const notFoundHandler = require('./middlewares/404');
 const errorHandler = require('./middlewares/500');
 const Protected = require('./middlewares/Protected');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit:'50mb'}));
 
 
 // app.get('/', (req,res) => {
 //     res.send("Welcome");
 // });
 
+app.use(userRouts);
+app.use(beneficiaryRouter);
+app.use(aboutUsRouts);
+app.use(paymentRouts);
+app.use(BlogRouts);
 app.use('*', notFoundHandler);
 app.use(errorHandler);
 app.use(Protected);
